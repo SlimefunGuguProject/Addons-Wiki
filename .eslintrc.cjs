4ca2098058a114faf7e2370926d4e03e81f19e40
ybw0014 <i@guizhanss.com>
2023-12-13 20:27:48 -0500

    feat: init starlight

---
+++ .eslintrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    '@guizhan/eslint-config'
+  ]
+}
---
+++ astro.config.mjs
@@ -0,0 +1,56 @@
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import tailwind from '@astrojs/tailwind'
+import remarkHeadingId from 'remark-heading-id'
+import rehypeExternalLinks from 'rehype-external-links'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: '粘液科技附属 中文Wiki',
+    description: '本站包含了粘液科技各大附属的中文Wiki。',
+    // logo: {
+    //   src: './src/assets/logo.png'
+    // },
+    // favicon: '/favicon.ico',
+    customCss: [
+      '@fontsource/noto-sans',
+      '@fontsource/noto-sans-sc',
+      './src/styles/docs.css'
+    ],
+    social: {
+      github: 'https://github.com/SlimefunGuguProject/Addons-Wiki'
+    },
+    defaultLocale: 'root',
+    locales: {
+      root: {
+        label: '简体中文',
+        lang: 'zh-CN'
+      }
+    },
+    head: [
+      // uncomment the following to enable cloudflare analytics
+      // {
+      //   tag: 'script',
+      //   attrs: {
+      //     src: 'https://static.cloudflareinsights.com/beacon.min.js',
+      //     'data-cf-beacon': '{"token": ""}',
+      //     defer: true
+      //   }
+      // }
+    ],
+    sidebar: [
+      // TODO: Replace sidebar with your own
+    ]
+  }), tailwind({
+    applyBaseStyles: false
+  })],
+  markdown: {
+    remarkPlugins: [
+      remarkHeadingId
+    ],
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+    ]
+  }
+})
